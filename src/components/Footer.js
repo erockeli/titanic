d2daa548a7c42a6c52768609b28cbb9cd96d8e9a
umekow <umekolashun@gmail.com>
2019-09-17 19:19:01 -0500

    Created Footer component. The component is empty, but it is ready to import into any other part of the app.

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = props =>{
+    return(
+        <div className='footer'>
+            {   
+            /*
+            *Code for footer goes here
+            */
+           }
+            
+        </div>
+    )
+}
+
+export default Footer
