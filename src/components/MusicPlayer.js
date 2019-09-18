8ec41f5483769b3faf1b0d92fef89ed27a358e2c
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-09-17 20:43:35 -0400

    Music Player Component Added

---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MusicPlayer = props => {
+    return (
+        <div className='music-player' key={props.id}>
+        <h1>Operation Titan Music Player</h1>
+        </div>
+    )
+}
+
+export default MusicPlayer;
