2301c40047f938ed5d30492cf48be60e81d127e0
umekow <umekolashun@gmail.com>
2019-09-17 19:39:04 -0500

    added a copyright at the bottom of footer of component. I also added a div with dummy contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,24 @@ const Footer = props =>{
             *Code for footer goes here
             */
            }
-            
+
+           <div className='contact'>
+               <p>Titantic@gmail.com</p>
+               <p>(123)456-7890</p>
+               <p></p>
+               <div className="social">
+                   {
+                       /*
+                        *Social media icons (regualar anchor tags?)
+                       */
+                   }
+               </div>
+           </div>
+
+            <div className='copyright'>
+                {/*copyright statement*/}
+                <small>&copy; Copyright 2019, Operation Titan</small>
+            </div>
         </div>
     )
 }
