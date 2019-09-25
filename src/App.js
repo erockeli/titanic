85065efb350aacf9e90399867876e02d0c5d3912
dbriksza <dbriksza@gmail.com>
2019-09-24 20:11:35 -0400

    Added some components to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-     <p>Hello this is my component for the coding challenge</p>
+      <NavBar />
+      <p>Hello this is my component for the coding challenge</p>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Footer = props =>{
-    return(
-        <div className='footer'>
-            {   
-            /*
-            *Code for footer goes here
-            */
-           }
+const Footer = props => {
+  return (
+    <div className="footer">
+      {/*
+       *Code for footer goes here
+       */}
 
-           <div className='contact'>
-               <p>Titanic@gmail.com</p>
-               <p>(123)456-7890</p>
-               <p></p>
-               <div className="social">
-                   {
-                       /*
-                        *Social media icons (regualar anchor tags?)
-                       */
-                   }
-               </div>
-           </div>
-
-            <div className='copyright'>
-                {/*copyright statement*/}
-                <small>&copy; Copyright 2019, Operation Titan</small>
-            </div>
+      <div className="contact">
+        <p>Titanic@gmail.com</p>
+        <p>(123)456-7890</p>
+        <p></p>
+        <div className="social">
+          {/*
+           *Social media icons (regualar anchor tags?)
+           */}
         </div>
-    )
-}
+      </div>
 
-export default Footer
+      <div className="copyright">
+        {/*copyright statement*/}
+        <p>&copy; Copyright 2019, Operation Titan</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
